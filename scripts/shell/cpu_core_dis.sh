5cc0347af832d5c84a376c6e2d50d1c083ac9d7f
naveen-fractlio <naveen.mh08@gmail.com>
2016-08-17 15:24:17 +0530

    Added CPU core disable script.

---
+++ scripts/shell/cpu_core_dis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Enable cores and Hiding unnecessary output to print on console menu
+    max=$(lscpu  | grep CPU\(s\) | head -n 1 | cut -d':' -f2 | rev)
+    dis="$(( $max -1))"
+    { for CPU in /sys/devices/system/cpu/cpu[$1-$dis]*; do
+        CPUID=$(basename $CPU)
+        #echo "CPU: $CPUID";
+        COREID="$(cat $CPU/topology/core_id)";
+        eval "COREENABLE=\"\${core${COREID}enable}\"";
+        #echo "$CPU core=${CORE} -> disable";
+        echo "1" > "$CPU/online";
+    done; } &> /dev/null
+#Disabling the cpu cores with the user input
+if [ "$1" -gt "1" ]; then
+    max=$(lscpu  | grep CPU\(s\) | head -n 1 | cut -d':' -f2 | rev)
+    dis="$(( $max -1))"
+    for CPU in /sys/devices/system/cpu/cpu[$1-$dis]*; do
+        CPUID=$(basename $CPU)
+        echo "CPU: $CPUID";
+	echo "Disabling this core..."
+        COREID="$(cat $CPU/topology/core_id)";
+        eval "COREENABLE=\"\${core${COREID}enable}\"";
+        echo "$CPU core=${CORE} -> disable";
+	echo
+        echo "0" > "$CPU/online";
+    done;
+	tc="$(( $1 - 1 ))"
+	echo "Cores successfully disabled. On-line CPU(s) are: [0-$tc]"
+	sleep 1
+else
+    echo "This input parameter is NOT VALID!"
+fi
