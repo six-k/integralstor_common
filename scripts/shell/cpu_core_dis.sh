4606a933db599843aae73fb760a492867fcf8624
naveen-fractlio <naveen.mh08@gmail.com>
2016-08-22 16:54:15 +0530

    Updated CPU core disable script.

--- scripts/shell/cpu_core_dis.sh
+++ scripts/shell/cpu_core_dis.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+TOTAL_CPU=$(nproc --all)
+echo
+echo "Total CPU(s):" $TOTAL_CPU
+
 #Enable cores and Hiding unnecessary output to print on console menu
     max=$(lscpu  | grep CPU\(s\) | head -n 1 | cut -d':' -f2 | rev)
     dis="$(( $max -1))"
@@ -11,9 +16,10 @@
         echo "1" > "$CPU/online";
     done; } &> /dev/null
 #Disabling the cpu cores with the user input
+
 if [ "$1" -gt "1" ]; then
-    max=$(lscpu  | grep CPU\(s\) | head -n 1 | cut -d':' -f2 | rev)
-    dis="$(( $max -1))"
+	max=$(lscpu  | grep CPU\(s\) | head -n 1 | cut -d':' -f2 | rev)
+	dis="$(( $max -1))"
     for CPU in /sys/devices/system/cpu/cpu[$1-$dis]*; do
         CPUID=$(basename $CPU)
         echo "CPU: $CPUID";
@@ -27,6 +33,15 @@ if [ "$1" -gt "1" ]; then
 	tc="$(( $1 - 1 ))"
 	echo "Cores successfully disabled. On-line CPU(s) are: [0-$tc]"
 	sleep 1
+elif [ "$1" -eq "1" ]; then
+	echo
+	echo "Resetting..."
+	TOTAL_CPU=$(nproc --all)
+	ONLINE=$(nproc)
+	sleep 1
+	echo
+	echo "Online CPU(s):" $ONLINE
+	echo
 else
-    echo "This input parameter is NOT VALID!"
+	echo "This input parameter is NOT VALID!"
 fi
